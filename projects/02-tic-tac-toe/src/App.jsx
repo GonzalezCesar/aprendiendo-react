18cc21589a1df0a105f671a7c2ede151ddd7f0dc
GonzalezCesar <gonzalez.abachecesar@gmail.com>
2024-01-28 14:18:18 -0400

    Starting second proyect

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -13,11 +13,22 @@ const Square = ({ children, isSelected, updateBoard, index}) => {
   }
 
   return (
-    <div onClick={handleClick} className="square">
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
 }
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
 
 function App() {
   const [board, setBoard] = useState(
@@ -26,10 +37,39 @@ function App() {
 
   const [turn, setTurn] = useState(TURNS.X)
 
-  const updateBoard = () => {
-    const newBoard = [... board]
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    // si no hay ganador
+    return null
+  }
+
+  const updateBoard = (index) => {
+    // no actualizamos esta posición
+    // si ya tiene algo
+    if (board[index] || winner) return
+    // actualizar el tablero
+    const newBoard = [ ... board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    // cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X 
     setTurn(newTurn)
+    //revisar si hay ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    }
   }
   
   return(
@@ -54,9 +94,10 @@ function App() {
       <section className="turn">
           <Square isSelected={turn === TURNS.X}>
             {TURNS.X}
-            </Square>
+          </Square>
           <Square  isSelected={turn === TURNS.O}>
-            {TURNS.O}</Square>
+            {TURNS.O}
+          </Square>
       </section>
     </main>
   )
