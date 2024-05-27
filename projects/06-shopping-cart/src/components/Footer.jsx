7f61c388f8ae1cb2fe354c720d5fdb844b7824bf
Gonzalez Cesar <gonzalez.abachecesar@gmail.com>
2024-05-27 15:47:09 -0400

    Shopping Cart done

---
+++ projects/06-shopping-cart/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import './Footer.css'
+
+export function Footer() {
+
+  return (
+    <footer className='footer'>
+        
+        <h4>Prueba tecnica de React 
+            <span>@midudev</span>
+        </h4>
+        <h5>
+            Shooping Cart con useContext & useReducer
+        </h5>
+    </footer>
+  )
+}
+
+ 
